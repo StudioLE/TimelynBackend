3032665a796808b9240aa5b7b3b9763e7510e7d5
Laurence Elsdon <git@elsdon.me>
2015-03-04 20:32:01 +0000

    Sails Installed

---
+++ README.md
@@ -0,0 +1,3 @@
+# timelinejs
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/DatesController.js
@@ -0,0 +1,11 @@
+/**
+ * DatesController
+ *
+ * @description :: Server-side logic for managing dates
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
