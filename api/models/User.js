0628b1a721a4941fce901247cba3613e096247db
Laurence Elsdon <git@elsdon.me>
2015-03-05 23:19:07 +0000

    Installed and ran sails-generate-auth

--- api/models/User.js
+++ api/models/User.js
@@ -1,23 +1,12 @@
-/**
-* User.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+var User = {
+  // Enforce model schema in the case of schemaless databases
+  schema: true,
 
-module.exports = {
-
-	attributes: {
-
-		// User data
-		email: 'string',
-		password: 'string',
-
-		// Sails associations
-		timelines: {
-			collection: 'timeline',
-			via: 'user'
-		}
-	}
+  attributes: {
+    username  : { type: 'string', unique: true },
+    email     : { type: 'email',  unique: true },
+    passports : { collection: 'Passport', via: 'user' }
+  }
 };
 
+module.exports = User;
