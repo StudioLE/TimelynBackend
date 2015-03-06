dd0926798f33bfd80d8dde45b7a17a77f36baaf6
Laurence Elsdon <git@elsdon.me>
2015-03-06 01:01:36 +0000

    Reverted back to Passportjs auth

--- api/models/User.js
+++ api/models/User.js
@@ -1,48 +1,12 @@
-/**
-* User.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+var User = {
+  // Enforce model schema in the case of schemaless databases
+  schema: true,
 
-module.exports = {
-
-	attributes: {
-
-		// User data
-		email: {
-			type: 'string',
-			unique: true,
-			required: true
-		},
-		password: {
-			type: 'string',
-			required: true,
-			minLength: 6
-		},
-		//token: 'string',
-
-		// Sails associations
-		timelines: {
-			collection: 'timeline',
-			via: 'user'
-		},
-
-		// Hash passwords on user creation
-		beforeCreate: function (attrs, next) {
-			var bcrypt = require('bcrypt');
-			
-			bcrypt.genSalt(10, function(err, salt) {
-				if (err) return next(err);
-
-				bcrypt.hash(attrs.password, salt, function(err, hash) {
-					if (err) return next(err);
-
-					attrs.password = hash;
-					next();
-				});
-			});
-		}
-	}
+  attributes: {
+    username  : { type: 'string', unique: true },
+    email     : { type: 'email',  unique: true },
+    passports : { collection: 'Passport', via: 'user' }
+  }
 };
 
+module.exports = User;
