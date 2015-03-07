3ebb6bdf7da3d55322dd5424f2c3f46f261b7f4d
Laurence Elsdon <git@elsdon.me>
2015-03-07 01:10:12 +0000

    TestController

---
+++ api/controllers/TestController.js
@@ -0,0 +1,11 @@
+/**
+ * TestController
+ *
+ * @description :: Server-side logic for managing tests
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
