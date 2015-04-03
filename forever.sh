11d38e60eebc150d28464415087a38c6af4f4fa4
Xysti Uno <xysti.uno@elsdon.me>
2015-04-03 22:20:48 +0100

    Added dev option to forever.sh

--- forever.sh
+++ forever.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-echo "Launching server forever"
-forever start -al timelyn/forever.log app.js
+echo "Is this a production server? [y/n]"
+
+read prod
+if [ $prod = y ]
+then
+  echo "Launching timelyn production server forever"
+  forever start -al timelyn/forever.log app.js --prod
+else
+  echo "Launching timelyn development server forever"
+  forever start -wal dev-timelyn/forever.log app.js
+fi
